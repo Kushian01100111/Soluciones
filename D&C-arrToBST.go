91ba7305260fd9fea8f202cd4dd468f37529581e
Kushian01100111 <comaspedro6@gmail.com>
2025-08-18 11:13:45 -0300

    18/08/2025 - Primer ejercicio incompleto de dividir y conquistar, terminar hoy si es posible o mañana

---
+++ D&C-arrToBST.go
@@ -0,0 +1,21 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func balancing(tree *TreeNode, l int) *TreeNode {
+	return nil
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	tree := &TreeNode{}
+
+	for i := 0; i < len(nums); i++ {
+
+	}
+
+	return tree
+}
